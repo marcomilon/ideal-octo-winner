65fa14284e6dc1801e1357abfd16831a7f130d6f
Marco Milon <marco@ctdevelopers.com>
2025-11-10 18:54:53 -0300

    Add example environment variables and display app title in H1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import Todos from './components/Todos.vue'
+import Todos from "./components/Todos.vue";
 </script>
 
 <template>
   <main>
+    <h1>{{ import.meta.env.VITE_APP_TITLE }}</h1>
     <Todos />
   </main>
 </template>
-
