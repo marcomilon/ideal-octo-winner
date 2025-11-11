49f91ea5c1899a59f286c60aa43f9aa9e2f60281
Marco Milon <marco@ctdevelopers.com>
2025-11-10 21:05:02 -0300

    Add lambda

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import type { Schema } from "./amplify/data/resource"
 import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api"
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
+
+const client = generateClient<Schema>()
+
+
+client.queries.sayHello({
+  name: "Amplify",
+})
 
 createApp(App).mount("#app");
